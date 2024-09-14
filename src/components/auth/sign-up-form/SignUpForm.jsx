5251f0c86b3d1b4af445e95e639736df67e8cad0
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-09-14 23:51:38 +0600

    Show Hide Password Mode is Added

--- src/components/auth/sign-up-form/SignUpForm.jsx
+++ src/components/auth/sign-up-form/SignUpForm.jsx
@@ -1,5 +1,10 @@
+"use client";
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+
 
 const SignUpForm = () => {
+  const [showPass, setShowPass] = useState(false);
   return (
     <div className="sign-up-form max-w-[600px] mx-auto">
       <form action="" className="flex flex-col gap-3">
@@ -10,7 +15,7 @@ const SignUpForm = () => {
             name="fullName"
             id="fullName"
             placeholder="Enter Your Name"
-            className="w-full outline-none border-2 border-transparent focus:border-green-400 bg-gray-100 px-1 py-1.5 rounded"
+            className="w-full outline-none border-2 border-transparent focus:border-green-400 bg-gray-100 p-2 rounded"
           />
         </div>
         <div className="flex flex-col gap-1">
@@ -20,25 +25,45 @@ const SignUpForm = () => {
             name="email"
             id="email"
             placeholder="Enter Your Email"
-            className="w-full outline-none border-2 border-transparent focus:border-green-400 bg-gray-100 px-1 py-1.5 rounded"
+            className="w-full outline-none border-2 border-transparent focus:border-green-400 bg-gray-100 p-2 rounded"
           />
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter Your Password"
-            className="w-full outline-none border-2 border-transparent focus:border-green-400 bg-gray-100 px-1 py-1.5 rounded"
-          />
+          <div className="relative">
+            <input
+              type={showPass ? "text" : "password"}
+              name="password"
+              id="password"
+              placeholder="Enter Your Password"
+              className="w-full outline-none border-2 border-transparent focus:border-green-400 bg-gray-100 p-2 rounded"
+            />
+            <div className="icon absolute top-[50%] right-2 translate-y-[-50%]">
+              <button
+              type="button"
+                onClick={() => setShowPass(true)}
+                className={showPass ? "hidden" : "inline-block"}
+              >
+                <FaRegEye />
+              </button>
+              <button
+              type="button"
+                onClick={() => setShowPass(false)}
+                className={showPass ? "inline-block" : "hidden"}
+              >
+                <FaRegEyeSlash />
+              </button>
+            </div>
+          </div>
         </div>
         <div className="btn-box mt-3 text-center">
-          <button className="bg-green-400 transition-all duration-300 hover:bg-green-500 active:bg-green-600 px-8 py-2 rounded text-white">Sign Up</button>
+          <button className="bg-green-400 transition-all duration-300 hover:bg-green-500 active:bg-green-600 px-8 py-2.5 rounded text-white">
+            Sign Up
+          </button>
         </div>
       </form>
     </div>
   );
-}
+};
 
-export default SignUpForm
+export default SignUpForm;
