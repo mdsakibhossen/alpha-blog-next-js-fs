75b0ae06675cb6dc45af1674e6bb741032ff0ae2
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-09-15 00:57:19 +0600

    Middleware is Added...

---
+++ src/middleware.js
@@ -0,0 +1,30 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export const config = {
+    matcher: [
+        "/dashboard/:path*",
+        "/admin/:path*",
+        "/api/admin/:path*",
+    ],
+};
+
+export default withAuth(
+    async function middleware(req) {
+        const url = req.nextUrl.pathname;
+        const isAdmin = req?.nextauth?.token?.user?.isAdmin;
+
+        if (url?.includes("/admin") && isAdmin) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    },
+    {
+        callbacks: {
+            authorized: ({ token }) => {
+                if (!token) {
+                    return false;
+                }
+            },
+        },
+    },
+);
