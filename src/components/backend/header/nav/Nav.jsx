2a1f3fa58a02402d5c9b3fee74948ab8299d9ab2
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-09-24 12:11:40 +0600

    Backend Navbar Profile is Completed

--- src/components/backend/header/nav/Nav.jsx
+++ src/components/backend/header/nav/Nav.jsx
@@ -1,17 +1,32 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import UserImage from "../../../../../public/images/user.webp";
 
 const Nav = () => {
+  const { data } = useSession();
   return (
     <nav>
       <div className="flex flex-col lg:flex-row justify-center items-center gap-1">
-        <div className="img-box w-10 h-10 rounded-full border border-green-400">
-          <Link href={"/profile"}>
-            {/* <Image /> */}
+        <div className="img-box w-10 h-10 rounded-full border border-green-400 overflow-hidden">
+          <Link href={"/dashboard/profile"}>
+            <Image
+              src={
+                data?.user?.image?.secure_url
+                  ? data?.user?.image?.secure_url
+                  : UserImage
+              }
+              alt={data?.user?.fullName || "Image"}
+              width={200}
+              height={200}
+              className="w-full h-full object-cover"
+              priority
+            />
           </Link>
         </div>
         <div className="name hidden lg:block">
-          <Link href={"/profile"}>Name</Link>
+          <Link href={"/dashboard/profile"}>{data?.user.fullName}</Link>
         </div>
       </div>
     </nav>
