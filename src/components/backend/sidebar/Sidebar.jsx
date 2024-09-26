2e90f7f4c7e502c57eb74feb0e5b50fb610b6a13
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-09-26 23:29:30 +0600

    Update Sidebar

--- src/components/backend/sidebar/Sidebar.jsx
+++ src/components/backend/sidebar/Sidebar.jsx
@@ -3,20 +3,30 @@
 import LogoutButton from "@/components/logout-button/LogoutButton";
 import NavLink from "@/components/nav-link/NavLink";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SlArrowRight } from "react-icons/sl";
 
 const Sidebar = () => {
   const { data } = useSession();
   const [sidebarOpened, setSidebarOpened] = useState(true);
 
+  useEffect(() => {
+    const sidebarChildren = document.querySelector("#sidebar").children;
+    Array.from(sidebarChildren).forEach((el) => {
+      el.addEventListener("click", () => setSidebarOpened(false));
+    });
+  }, []);
+  
   return (
     <div
       className={`fixed ${
         sidebarOpened ? "translate-x-0" : "translate-x-[-100%]"
       } h-[85vh] xl:static left-0 transition-all duration-300`}
     >
-      <aside className="min-w-[200px] min-h-full bg-slate-200 p-3 overflow-y-auto flex flex-col gap-1.5 font-medium text-lg relative">
+      <aside
+        id="sidebar"
+        className="min-w-[200px] min-h-full bg-slate-200 p-3 overflow-y-auto flex flex-col gap-1.5 font-medium text-lg relative"
+      >
         <button
           onClick={() => setSidebarOpened(!sidebarOpened)}
           className={`fixed top-[50%] translate-y-[-50%] right-0 translate-x-[60%] bg-slate-200 w-10 h-14 rounded-full flex justify-center items-center text-3xl xl:hidden`}
