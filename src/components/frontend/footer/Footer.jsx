8b336b1c219c1b82ae93261ae613c1e1b7242074
Md Sakib Hossen <sakib.hossen0143@gmail.com>
2024-09-26 22:38:47 +0600

    Update footer: Categories

--- src/components/frontend/footer/Footer.jsx
+++ src/components/frontend/footer/Footer.jsx
@@ -1,6 +1,10 @@
+"use client";
+import { useGetAllCategoriesQuery } from "@/redux/services/category/categoryApi";
 import Link from "next/link";
 
 const Footer = () => {
+  const { data: cats = [] } = useGetAllCategoriesQuery();
+  const categories = cats.filter((_, i) => i < 5);
   return (
     <footer className="bg-slate-200">
       <div className="container mx-auto px-3 py-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10">
@@ -16,29 +20,44 @@ const Footer = () => {
           <h3 className="font-medium text-lg mb-2.5">Quick Links</h3>
           <div className="links flex flex-col gap-2">
             <Link href={"/"}>Home</Link>
-            <Link href={"/"}>About</Link>
+            <Link href={"/about-us"}>About</Link>
             <Link href={"/"}>Blog</Link>
-            <Link href={"/"}>Contact</Link>
-            <Link href={"/"}>Author</Link>
+            <Link href={"/contact-us"}>Contact</Link>
           </div>
         </div>
         <div className="col">
           <h3 className="font-medium text-lg mb-2.5">Category</h3>
           <div className="links flex flex-col gap-2">
-            <Link href={"/"}>Lifestyle</Link>
+            {categories &&
+              categories.map((category) => (
+                <Link key={category?.key} href={`/${category?.slug}`}>
+                  {category?.title}
+                </Link>
+              ))}
+            {/* <Link href={"/"}>Lifestyle</Link>
             <Link href={"/"}>Technology</Link>
             <Link href={"/"}>Travel</Link>
             <Link href={"/"}>Business</Link>
-            <Link href={"/"}>Sports</Link>
+            <Link href={"/"}>Sports</Link> */}
           </div>
         </div>
         <div className="col">
           <div className="card bg-white p-4 rounded">
-            <h3 className="font-medium text-lg text-center">Weekly Newsletter</h3>
-            <p className="text-slate-300 text-center">Get blog articles and offers via email</p>
+            <h3 className="font-medium text-lg text-center">
+              Weekly Newsletter
+            </h3>
+            <p className="text-slate-300 text-center">
+              Get blog articles and offers via email
+            </p>
             <div className="mt-5">
-              <input type="email" className="w-full bg-gray-200 px-3 py-2 focus:outline-green-500 rounded" placeholder="Your Email" />
-              <button className="w-full py-2 bg-blue-600 mt-3 rounded text-white">Subscribe</button>
+              <input
+                type="email"
+                className="w-full bg-gray-200 px-3 py-2 focus:outline-green-500 rounded"
+                placeholder="Your Email"
+              />
+              <button className="w-full py-2 bg-blue-600 mt-3 rounded text-white">
+                Subscribe
+              </button>
             </div>
           </div>
         </div>
